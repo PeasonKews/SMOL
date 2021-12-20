128858a8f92308acdb1844bc4289055a0b3fd32a
Jstodd <65479705+j-stodd@users.noreply.github.com>
2021-12-20 11:13:27 -0600

    Add files via upload
    Expanded word list

--- utils.js
+++ utils.js
@@ -1,3 +1,54 @@
+// LZW-compress a string
+function lzw_encode(s) {
+  var dict = {};
+  var data = (s + "").split("");
+  var out = [];
+  var currChar;
+  var phrase = data[0];
+  var code = 256;
+  for (var i=1; i<data.length; i++) {
+    currChar=data[i];
+    if (dict[phrase + currChar] != null) {
+      phrase += currChar;
+    } else {
+      out.push(phrase.length > 1 ? dict[phrase] : phrase.charCodeAt(0));
+      dict[phrase + currChar] = code;
+      code++;
+      phrase=currChar;
+    };
+  };
+  out.push(phrase.length > 1 ? dict[phrase] : phrase.charCodeAt(0));
+  for (var i=0; i<out.length; i++) {
+    out[i] = String.fromCharCode(out[i]);
+  };
+  return out.join("");
+};
+
+// Decompress an LZW-encoded string
+function lzw_decode(s) {
+  var dict = {};
+  var data = (s + "").split("");
+  var currChar = data[0];
+  var oldPhrase = currChar;
+  var out = [currChar];
+  var code = 256;
+  var phrase;
+  for (var i=1; i<data.length; i++) {
+    var currCode = data[i].charCodeAt(0);
+    if (currCode < 256) {
+      phrase = data[i];
+    } else {
+      phrase = dict[currCode] ? dict[currCode] : (oldPhrase + currChar);
+    };
+    out.push(phrase);
+    currChar = phrase.charAt(0);
+    dict[code] = oldPhrase + currChar;
+    code++;
+    oldPhrase = phrase;
+  };
+  return out.join("");
+};
+
 function byteCount(s) {
     return encodeURI(s).split(/%(?:u[0-9A-F]{2})?[0-9A-F]{2}|./).length - 1;
 };
@@ -13,7 +64,9 @@ function SMOL(str, dir, key, nSize){
   if (dir >= 1){
     str = compress(str);
     if (key) str = encrypt(str, key, nSize);
+    //str = lzw_encode(str);
     } else {
+      //str = lzw_decode(str);
       if (key) str = decrypt(str, key, nSize);
       str = decompress(str);
     };
