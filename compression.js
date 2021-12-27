736d5cb1baff4c508fa9bff9050285a5fead055b
Jstodd <65479705+j-stodd@users.noreply.github.com>
2021-12-27 12:59:53 -0600

    Update compression.js

--- compression.js
+++ compression.js
@@ -196,17 +196,7 @@ function decompress(str, key, nSize){
 };
 
 function compress(str, key, nSize){
-  str = convertToBin(str);
-  /*//Test
-  let testStr = str;
-  console.log(testStr)
-  while (testStr){
-    console.log(testStr.substring(0,13), convBin2Text[testStr.substring(0,13)].text);
-    testStr = testStr.substring(13);
-  };*/
-  
-  //console.log(binary13[allItems.length])
-  
+  str = convertToBin(str);  
   str = convertToBase64(str);
   if (key){
     key = formatKey(key);
